4edd3629d3db1170bf81b6bcd9b473ebb7453a4c
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2026-01-12 18:25:47 +0600

    lern primitive and non primitive data type

---
+++ modeule1/src/basic.ts
@@ -0,0 +1,38 @@
+//Basic Data Type
+
+// String
+
+let fatherName:String = 'Abu Haydar Ali'
+
+// number 
+let age : number = 56;
+
+//boolean 
+let isAdmin : boolean = true;
+
+//undefined
+let x : undefined = undefined;
+
+//null 
+let y : null = null;
+
+
+//Array
+
+let friends : string[] = ['alif', 'medul', 'arfan', 'lefan','saiful']
+
+friends.push("valluk")
+
+
+
+let rollList : number[] = [23,35,34,6,]
+rollList.push(4)
+
+
+//tuple ----> special array ------> order mintance -----> type of values
+
+
+let coordinates : [number,number] = [3,7];
+
+
+let ageName : [number ,string] = [34,"mokbul"]
