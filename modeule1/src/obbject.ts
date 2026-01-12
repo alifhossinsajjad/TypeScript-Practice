0fb465fe1e477a76d293f12dbecc1f7af448f147
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2026-01-12 23:29:55 +0600

    how to declear object

---
+++ modeule1/src/obbject.ts
@@ -0,0 +1,14 @@
+//Reference Type -----------> Object
+
+const user: {
+    readonly company: string; //type --------> literal types
+    fristName: string;
+    middleName?: string // optional Type
+    lastName: string
+    isMarried: boolean
+} = {
+    company : "google",
+    fristName: "alif ",
+    lastName: "mia",
+    isMarried: false
+}
