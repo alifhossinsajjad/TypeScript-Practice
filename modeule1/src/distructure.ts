25a939500bee332ebc91985b53a7d78214869980
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2026-01-13 13:34:34 +0600

    how to destrucutre array and object;

---
+++ modeule1/src/distructure.ts
@@ -0,0 +1,21 @@
+//destructure operator 
+//object destructure
+const user = {
+    id: 234,
+    name: {
+        fristName: "alif",
+        lastname: "mia"
+    },
+    contactNom: "018234874",
+}
+
+const { contactNom, name: { fristName } } = user
+
+
+//array destructure 
+
+const myfriends = ['alif', 'arfan', 'midul', 'biri','lifan'];
+
+
+
+const [, , valobasa, ...smoking]  = myfriends
