cebfb6db4cb68b26137e4010782b46dc83ee1845
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2026-01-15 16:29:55 +0600

    HOW TO USE UNION OPERATOR

---
+++ PractiseTask/task7.ts
@@ -0,0 +1,21 @@
+/* Task 7: Type Assertion and Narrowing
+Objective: Write a function that behaves differently based on the input type.
+
+Instructions:
+
+Create a function that accepts a parameter of type string | number.
+The function should:
+Return the length if the input is a string.
+Return the square if the input is a number. */
+
+
+function processInput (inp : string | number) : number {
+if(typeof inp === "string") {
+    return inp.length
+}else{
+    return inp *inp
+}
+}
+
+
+console.log(processInput(8));
